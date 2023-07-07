d75873373946acc53cd47ec30750d4319f967d7c
ashishrout-tech <ashishrout655@gmail.com>
2023-07-07 14:46:57 +0530

    Changed All object components to function components And fixed some minor issues

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import './App.css';
-
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
-  pageSize = 5;
-  apiKey = process.env.REACT_APP_NEWS_API
-  state = {
-    progress: 0
-  }
+const App = () => {
+  const pageSize = 5;
+  const apiKey = process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0);
 
-  setProgress=(progress)=>{
-    this.setState({progress: progress})
-  }
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <LoadingBar color='#f11946' height={3} progress={this.state.progress}/>
-        <News setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="general" />
-        {/* <News pageSize={this.pageSize} country = "in" category="business"/>
+  // const setProgress = (progress) => {
+  //   setPgs(progress);
+  // }
+  return (
+    <div>
+      <NavBar />
+      <LoadingBar color='#f11946' height={3} progress={progress} />
+      <News setProgress={setProgress} apiKey={apiKey} pageSize={pageSize} country="in" category="science" />
+      {/* <News pageSize={this.pageSize} country = "in" category="business"/>
         <News pageSize={this.pageSize} country = "in" category="entertainment"/>
         <News pageSize={this.pageSize} country = "in" category="general"/>
         <News pageSize={this.pageSize} country = "in" category="health"/>
         <News pageSize={this.pageSize} country = "in" category="science"/>
         <News pageSize={this.pageSize} country = "in" category="sports"/>
         <News pageSize={this.pageSize} country = "in" category="technology"/> */}
-      </div>
-    )
-  }
+    </div>
+  )
 }
+export default App;
 
--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
@@ -6,64 +6,43 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 
 
-export class News extends Component {
-  static defaultProps = {
-    country: 'in',
-    pageSize: 8,
-    category: "general",
-  }
+const News = (props) => {
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResults] = useState(0)
+  const [defaultImg, setDefaultImg] = useState("https://th.bing.com/th/id/OIP.mdvZkRzpsDq0bIhPPaDZSAHaHa?pid=ImgDet&rs=1");
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  }
-
-  default = "https://th.bing.com/th/id/OIP.mdvZkRzpsDq0bIhPPaDZSAHaHa?pid=ImgDet&rs=1";
-
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      defaultImg: this.default,
-      loading: false,
-      page: 1,
-      totalResults: 0
+  useEffect(() => {
+    // let ignore = false;
+    async function fetchData() {
+      // if (!ignore) {
+      props.setProgress(10);
+      // this.updateNews();
+      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+      setLoading(true);
+      let data = await fetch(url);
+      props.setProgress(30);
+      let parsedData = await data.json()
+      props.setProgress(60);
+      setArticles(parsedData.articles);
+      setTotalResults(parsedData.totalResults);
+      setLoading(false);
+      props.setProgress(100);
+      // }
     }
-  }
+    fetchData();
+    return () => {
+      setPage(page + 1);
+      // ignore = true;
+      // ignore can be used for repeated calls (it's called when there's a change in param passed to it inside the [] braces, but in this case it's called twice because of strict mode, In the first call it accesses the fetchData(), but don't get inside cleanup function. In the second call, cleanup function is called and then the fetchData()). So, the cleanup function is called once.
+    }
 
-  // async updateNews() {
-  //   // console.log(this.state.page);
-  //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-  //   this.setState({ loading: true })
-  //   let data = await fetch(url);
-  //   let parsedData = await data.json()
-  //   this.setState({
-  //     articles: parsedData.articles,
-  //     totalResults: parsedData.totalResults,
-  //     loading: false,
-  //     page: this.state.page + 1
-  //   })
-  // }
+  }, [])
 
-  async componentDidMount() {
-    this.props.setProgress(10);
-    // this.updateNews();
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true })
-    let data = await fetch(url);
-    this.props.setProgress(30);
-    let parsedData = await data.json()
-    this.props.setProgress(60);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    })
-    this.props.setProgress(100);
-  }
 
-  // handlePrevClick = async () => {
+
+  // {// handlePrevClick = async () => {
   //   await this.setState({
   //     page: this.state.page - 1
   //   })
@@ -75,48 +54,59 @@ export class News extends Component {
   //     page: this.state.page + 1
   //   })
   //   this.updateNews();
-  // }
+  // }}
 
-  fetchMoreData = async () => {
-    ++this.state.page;
-    console.log(this.state.page)
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true })
+
+
+  const fetchMoreData = async () => {
+    // console.log(`Before ${page}`); // same value as below
+
+    setPage(page + 1); // here, it doesn't instantly increase the value by 1. It informs for the increment, and then moves onto next line. The result is seen in the next call. So, it's of no use to call set function at the beginning.
+    //else in smart way, we can dircetly pass page+1 in &{page} in url below.
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-      loading: false,
-    })
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults)
+
+    // console.log(`After ${page}`) // same value as above
   }
 
-  render() {
+  return (
+    <>
+      <h1 className='text-center' style={{ margin: '35px', marginTop: '90px' }}>NewsApp- Top Headlines</h1>
+      {/* {this.state.loading && <Spinner />} */}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
 
-    return (
-      <>
-        <h1 className='text-center my-5'>NewsApp- Top Headlines</h1>
-        {/* {this.state.loading && <Spinner />} */}
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container">
-            
-            <div className="row">
-              {this.state.articles.map((element) => {
-                return <div className="col-md-4" key={element.url}>
-                  <NewsItem title={element.title ? element.title.slice(0, 45) + "..." : ""} description={element.description ? element.description.slice(0, 88) + "..." : ""} imageUrl={element.urlToImage ? element.urlToImage : this.state.defaultImg} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                </div>
-              })}
-            </div>
+          <div className="row">
+            {articles.map((element) => {
+              return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title ? element.title.slice(0, 45) + "..." : ""} description={element.description ? element.description.slice(0, 88) + "..." : ""} imageUrl={element.urlToImage ? element.urlToImage : defaultImg} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+              </div>
+            })}
           </div>
-        </InfiniteScroll>
-      </>
-    )
-  }
+        </div>
+      </InfiniteScroll>
+    </>
+  )
+}
+
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: "general",
+}
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
 }
 
 export default News
